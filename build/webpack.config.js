452b100714e358db72a7611d85c898284d697550
nobug233@163.com <nobug233@163.com>
2024-12-16 10:49:03 +0800

    1111

--- build/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './dist/index.js',
-  output: {
-    path: path.resolve(process.cwd(), './dist/'),
-    filename: 'bundle.js',
-  },
-};
